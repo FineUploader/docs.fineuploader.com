2829e2f381cb32647c1a5fc0e6640946fc553964
Ray Nicholus <rnicholus@users.noreply.github.com>
2017-02-26 22:41:49 -0600

    backfill tags from dreamhost server

--- branch/docs_tag-chooser/_static/js/navbar.js
+++
@@ -1,9 +0,0 @@
-(function() {
-    document.querySelector('.tag-chooser').addEventListener('submit', function(event) {
-        event.preventDefault()
-        var version = event.target.elements['tag'].value
-        if (version.trim()) {
-            window.location.href = '/tag/' + version
-        }
-    })
-}())
